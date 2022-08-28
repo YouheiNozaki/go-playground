6f51771915abd73d21073d18c76937d84442f84a
Youhei Nozaki <youhey.hey0505@gmail.com>
2022-08-28 22:12:33 +0900

    concurrency

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,13 +1,25 @@
 package concurrency
 
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
-    results := make(map[string]bool)
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
-    for _, url := range urls {
-        results[url] = wc(url)
-    }
+	for _, url := range urls {
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
 
-    return results
+	for i := 0; i < len(urls); i++ {
+		result := <- resultChannel
+		results[result.string] = result.bool
+	}
+
+	return results
 }
